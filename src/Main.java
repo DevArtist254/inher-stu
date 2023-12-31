bbfdd65a568adf332a728b630cd5a4200b205590
DevArtist <kevinwasonga415@outlook.com>
2023-12-31 12:38:57 +0300

    inhert

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Animal animal = new Animal("Generic Animal", "Huge", 400);
+
+        doAnimalStuff(animal, "slow");
+
+        Dog dog = new Dog();
+
+        doAnimalStuff(dog, "fast");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed){
+
+        animal.makesNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("____");
     }
 }
